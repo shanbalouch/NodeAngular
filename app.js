3fb187e80f567c69d65ade660363d9666f812499
shanBalouch <shanlashari@changamanga.org>
2017-09-27 12:34:46 +0500

    JWT Awsome

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 
 // Passport Middleware
 app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.session());
 
 require('./config/passport')(passport);
 
